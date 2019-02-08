92a4f09b53c5d197a3147d4c19fad71eebed778f
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-02-08 16:36:00 -0600

    Update EmployeeUserInterface.java
    This is closer to the final form - the button actually does what it's supposed to do: scrape the JTextFields for data, format that data properly, construct an Employee object, use that temporary Employee's raiseSalary() method, and display the raise in a JOptionPane.

--- EmployeeUserInterface.java
+++ EmployeeUserInterface.java
@@ -3,30 +3,54 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class EmployeeUserInterface extends JFrame {
+	
 	//JLabel and JTextField for Name
 	private JLabel employeeName;
 	private JTextField nameField;
+	
 	//JLabel and JTextField for Title
 	private JLabel employeeTitle;
 	private JTextField titleField;
+	
 	//JLabel and JTextField for Age
 	private JLabel employeeAge;
 	private JTextField ageField;
+	
 	//JLabel and JTextField for Salary
 	private JLabel employeeSalary;
 	private JTextField salaryField;
+	
 	//JLabel and JTextField for Percent
 	private JLabel raisePercent;
 	private JTextField percentField;
+	
 	//JButton for starting calculation and JLabel for displaying raise
 	private JButton calculate;
-	private JLabel calculatedRaise;
+	
+	//Variables for handling input from JTextFields
+	private String name;
+	private String title;
+	private String age;
+	private String salary;
+	private String percent;
+	private String showRaise;
+	
+	//This set of variables is for casting and storing the numeric data types 
+	//necessary for constructing an Employee object
+	private int numAge;
+	private double numSalary;
+	private int numPercent;
+	
+	//JFrame needed for our calculation pop-up
+	final JFrame parent = new JFrame();
 	
 	//Constructor method
 	public EmployeeUserInterface() {
+		//Set up our frame and layout
 		super("Employee");
 		setLayout(new GridLayout(6, 3));
 		
+		//Here we the define the JLabels and JTextFields that we'll need
 		employeeName = new JLabel("Name");
 		nameField = new JTextField("Name", 20);
 		
@@ -42,9 +66,10 @@ public class EmployeeUserInterface extends JFrame {
 		raisePercent = new JLabel("Raise %");
 		percentField = new JTextField("Raise %", 20);
 		
+		//Here we define the JButton we'll need
 		calculate = new JButton("Calculate");
-		calculatedRaise = new JLabel(" ");
-		
+
+		//And now we add everything to the frame!
 		add(employeeName);
 		add(nameField);
 		add(employeeTitle);
@@ -56,17 +81,28 @@ public class EmployeeUserInterface extends JFrame {
 		add(raisePercent);
 		add(percentField);
 		add(calculate);
-		add(calculatedRaise);
 		
-	}
+		//And here we implement the logic for the calculate button.
+		//When a user clicks the button, it will dynamically create an Employee object with data from the text fields.
+		//It will then use that new Employee's raiseSalary() method to generate a raised salary, which will be stored and displayed 
+		//in a JOptionPane.
+		calculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 
-	//Event handler method - will need to generate Employee object for calculation
-	//Will include error handling... perhaps seperate method?
-	private class HandlerClass implements ItemListener{
-		public void itemStateChanged(ItemEvent event) {
+				name = nameField.getText();
+				title = titleField.getText();
+				salary = salaryField.getText();
+				numSalary = Double.parseDouble(salary);
+				age = ageField.getText();
+				numAge = Integer.parseInt(age);
+				percent = percentField.getText();
+				numPercent = Integer.parseInt(percent);
+				
+				Employee employee = new Employee(name, title, numSalary, numAge);
+				showRaise = "Salary Raised! " + Double.toString(employee.raiseSalary(numPercent));
+				JOptionPane.showMessageDialog(parent, showRaise);
+			}
+		});
 		
-		}
 	}
 	
-	
-}
